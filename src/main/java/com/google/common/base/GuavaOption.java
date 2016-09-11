7834c264dc98c9567141447dc758db7171415381
Kristian Kime <kristian.kime@gmail.com>
2016-09-10 21:49:51 -0400

    fixing javadoc compliation

--- src/main/java/com/google/common/base/GuavaOption.java
+++ src/main/java/com/google/common/base/GuavaOption.java
@@ -3,6 +3,12 @@ package com.google.common.base;
 import com.artclod.common.collect.base.Option;
 import com.google.common.base.Optional;
 
+/**
+ * This class is both a Guava {@link com.google.common.base.Optional} and an {@link com.artclod.common.collect.base.Option}.
+ * This can be used both FCollections and Guava functionality is desired.
+ *
+ * @param <T> Type of potential value in the option
+ */
 public abstract class GuavaOption<T> extends Optional<T> implements Option<T> {
 	private static final long serialVersionUID = 1L;
 
